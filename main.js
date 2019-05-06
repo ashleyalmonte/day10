3b730b0a09990aa77787d624d289a0f274be83e5
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-06 12:32:11 -0400

    ughhhhhhhh

---
+++ main.js
@@ -0,0 +1,21 @@
+  console.log('Hello World')
+
+let answer= prompt('how are you doing?').toLowercase()
+let goodWords=['good', 'well', ]
+let badWords=['chill', 'bad', 'meh',]
+
+function doingWell(ans) {
+  if(goodWords.incloude(ans)) {
+    return true
+  }
+}
+
+if (answer ==) {
+   console.log('That\'s great!')
+   let answer2 = prompt('WHere are you from?').toLowercase()
+   if (answer2 == 'smashville') {
+     console.log(YER)
+   }
+ } else if (answer == "bad") {
+   console.log("01' GOtchu huh")
+ }
